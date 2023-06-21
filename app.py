39e58b0b8f4060aa8f0cc845d9069b302090f1fe
danettp <17204@my.sanctamaria.school.nz>
2023-06-22 10:37:29 +1200

    edited

--- app.py
+++ app.py
@@ -4,5 +4,5 @@ if __name__ == "__main__":
     app = create_app()  # Create a Flask application instance using the create_app function
     app.run(debug=True) # Run the Flask application in debug mode
 
-# Fixing email validation with WTforms - Signup Page Part 1
+# 13:46 Fixing email validation with WTforms - Signup Page Part 3
 # Add comments to code
